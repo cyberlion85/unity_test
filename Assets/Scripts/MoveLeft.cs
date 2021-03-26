044a6fa324cc6e55e652873136afd13871c1c715
Stanislav Persanov <s.persanov@gmail.com>
2021-03-26 15:36:50 +0300

    unit 3 finish

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 15;
+    private float leftBound = -15;
     private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,10 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 }
