f99100d81954c8314c4af2ec122ed904b60c7f34
Stanislav Persanov <s.persanov@gmail.com>
2021-03-29 13:12:31 +0300

    4.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,57 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    private Animator playerAnim;
-    private AudioSource playerAudio;
-    public ParticleSystem explosionParticle;
-    public ParticleSystem dirtPartice;
-    public AudioClip jumpSound;
-    public AudioClip crashSound;
-    public float gravityModifier;
-    public float jumpForce=10;
-    public bool isOnGround = true;
-    public bool gameOver;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerAnim = GetComponent<Animator>();
-        playerAudio = GetComponent<AudioSource>();
-        Physics.gravity *= gravityModifier;
+        focalPoint = GameObject.Find("Focal Point");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
-        {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-            playerAnim.SetTrigger("Jump_trig");
-            dirtPartice.Stop();
-            playerAudio.PlayOneShot(jumpSound, 1.0f);
-
-        }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-            dirtPartice.Play();
-
-            Debug.Log("ground");
-        }
-        else if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            Debug.Log("GAME OVER");
-            gameOver = true;
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
-            explosionParticle.Play();
-            playerAudio.PlayOneShot(crashSound, 1.0f);
-
-        }
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
